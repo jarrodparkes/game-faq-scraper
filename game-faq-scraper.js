fead8365f5979ff738cec5449d577c8d4224558c
Jarrod Parkes <parkesfjarrod@gmail.com>
2019-03-22 00:18:14 +0900

    finish scraping basic game data

--- game-faq-scraper.js
+++ game-faq-scraper.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 const longWait = 8000;
 const mediumWait = 4000;
@@ -13,6 +14,9 @@ async function getElementText(page, query) {
 }
 
 async function run() {
+	// empty details string
+	var details = '';
+
   // create browser, open a tab, and set the window size
   const browser = await puppeteer.launch({ headless: headless });
   const page = await browser.newPage();
@@ -23,23 +27,29 @@ async function run() {
 
 	// get title
 	const title = await getElementText(page, '//*[@id="content"]/div/div/header/div/div[2]/h1');
-	console.log('Title: ' + title);
+	details += 'Title: ' + title + '\n';
 
   // get platform
 	const platform = await getElementText(page, '//div[contains(@class, "pod_gameinfo")]/div[contains(@class, "body")]/ul/li[contains(., "Platform")]/a');
-	console.log('Platform: ' + platform);
+	details += 'Platform: ' + platform + '\n';
 
   // get developer
 	const developer = await getElementText(page, '//div[contains(@class, "pod_gameinfo")]/div[contains(@class, "body")]/ul/li[contains(., "Developer")]/a');
-	console.log('Developer: ' + developer);
+	details += 'Developer: ' + developer + '\n';
 
   // get publisher
 	const publisher = await getElementText(page, '//div[contains(@class, "pod_gameinfo")]/div[contains(@class, "body")]/ul/li[contains(., "Publisher")]/a');
-	console.log('Publisher: ' + publisher);
+	details += 'Publisher: ' + publisher + '\n';
 
 	// get release date
 	const releaseDate = await getElementText(page, '//div[contains(@class, "pod_gameinfo")]/div[contains(@class, "body")]/ul/li[contains(., "Release")]/a');
-	console.log('Release Date: ' + releaseDate);
+	details += 'Release Date: ' + releaseDate + '\n';
+
+	// write details to a file
+	fs.writeFile("game-details.txt", details, function(err) {
+		if (err) { return console.log(err); }
+	    console.log("The file was saved!");
+	});
 
   // close the browser
 	await page.waitFor(shortWait);
