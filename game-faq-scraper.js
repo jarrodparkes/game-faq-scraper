57bebc879e6fb312621d45b297b7b2a43d2b652a
Jarrod Parkes <parkesfjarrod@gmail.com>
2019-03-22 00:15:37 +0900

    finish scraping basic game data

--- game-faq-scraper.js
+++ game-faq-scraper.js
@@ -1,11 +1,16 @@
 const puppeteer = require('puppeteer');
-const request = require ("request");
 
 const longWait = 8000;
 const mediumWait = 4000;
 const shortWait = 1000;
 
-const headless = true;
+const headless = false;
+
+async function getElementText(page, query) {
+	const element = (await page.$x(query))[0];
+  const innerText = await (await element.getProperty('innerText')).jsonValue();
+  return innerText;
+}
 
 async function run() {
   // create browser, open a tab, and set the window size
@@ -13,7 +18,32 @@ async function run() {
   const page = await browser.newPage();
   if (headless === false) { await page.setViewport({width: 1200, height: 600}); }
 
-	// start here!
+	// open game summary page
+  await page.goto('https://gamefaqs.gamespot.com/n64/196694-banjo-kazooie', { waitUntil: 'networkidle0' });
+
+	// get title
+	const title = await getElementText(page, '//*[@id="content"]/div/div/header/div/div[2]/h1');
+	console.log('Title: ' + title);
+
+  // get platform
+	const platform = await getElementText(page, '//div[contains(@class, "pod_gameinfo")]/div[contains(@class, "body")]/ul/li[contains(., "Platform")]/a');
+	console.log('Platform: ' + platform);
+
+  // get developer
+	const developer = await getElementText(page, '//div[contains(@class, "pod_gameinfo")]/div[contains(@class, "body")]/ul/li[contains(., "Developer")]/a');
+	console.log('Developer: ' + developer);
+
+  // get publisher
+	const publisher = await getElementText(page, '//div[contains(@class, "pod_gameinfo")]/div[contains(@class, "body")]/ul/li[contains(., "Publisher")]/a');
+	console.log('Publisher: ' + publisher);
+
+	// get release date
+	const releaseDate = await getElementText(page, '//div[contains(@class, "pod_gameinfo")]/div[contains(@class, "body")]/ul/li[contains(., "Release")]/a');
+	console.log('Release Date: ' + releaseDate);
+
+  // close the browser
+	await page.waitFor(shortWait);
+  await browser.close();
 }
 
 run();
